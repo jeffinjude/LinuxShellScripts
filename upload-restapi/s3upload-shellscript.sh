4891296a00733cdf129e32d0ad0e40dd6beb83db
Ubuntu <ubuntu@ip-172-31-8-53.us-west-1.compute.internal>
2017-02-03 11:26:33 +0000

    uploading all files in a dir

--- upload-restapi/s3upload-shellscript.sh
+++ upload-restapi/s3upload-shellscript.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
-# Shell script to upload file to aws s3 bucket
-
-current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-objectName=sample-text_$current_time.txt
-file=sample-text.txt
-bucket=jeffin-files
-resource="/${bucket}/${objectName}"
-contentType="text/plain"
-dateValue=`date -R`
-stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+# Shell script to upload files to aws s3 bucket
 
 # Fetch the  aws access keys from the user
 read -p "Please provide your aws access key : " s3Key
 read -p "Please provide your aws secret access key : " s3Secret
 
-# Prepare the authorization header string
-signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+# Directory where the files to upload are present
+file_path="/home/ubuntu/shell-scripts/upload-restapi/files-to-upload"
 
-# S3 bucket Rest api to upload file
-curl -v -i -X PUT -T "${file}" \
+# Function to upload the files to s3 bucket using rest api
+function uploadToS3
+{
+	objectName=$1
+	bucket=jeffin-files
+	resource="/${bucket}/${objectName}"
+	contentType="text/plain"
+	dateValue=`date -R`
+	stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+
+	# Prepare the authorization header string
+	signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+
+	# S3 bucket Rest api to upload file
+	curl -v -i -X PUT -T "${file_path}/${objectName}" \
           -H "Host: ${bucket}.s3.amazonaws.com" \
           -H "Date: ${dateValue}" \
           -H "Content-Type: ${contentType}" \
           -H "Authorization: AWS ${s3Key}:${signature}" \
           https://${bucket}.s3.amazonaws.com/${objectName}
+}
+
+for file in "$file_path"/*; do
+	uploadToS3 "${file##*/}"
+done
